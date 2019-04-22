1c3addbcad4f7f65755102f3e8a7104ca3eeb311
HussainBee <haya.haseen2@gmail.com>
2019-04-22 16:58:25 -0700

    Testing example

---
+++ Gproj2/src/com/kamal/Testing.java
@@ -0,0 +1,10 @@
+package com.kamal;
+
+public class Testing {
+
+	public static void main(String[] args) {
+	System.out.println("hello every one am kamal bee shaik from spps systems pioneer soft pvt ltd...");
+
+	}
+
+}
