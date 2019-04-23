40be68e39f093a1f33093b65b504139bd41ef975
HussainBee <haya.haseen2@gmail.com>
2019-04-22 17:15:37 -0700

    simplying message

--- Gproj2/src/com/kamal/Testing.java
+++ Gproj2/src/com/kamal/Testing.java
@@ -3,7 +3,7 @@ package com.kamal;
 public class Testing {
 
 	public static void main(String[] args) {
-	System.out.println("hello every one am kamal bee shaik from spps systems pioneer soft pvt ltd...");
+	System.out.println("hello every one ");
 
 	}
 
