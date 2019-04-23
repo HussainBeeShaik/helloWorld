5f8ae8406b2d85f9e37d30a288fc8c9280ad8173
HussainBee <haya.haseen2@gmail.com>
2019-04-22 17:23:52 -0700

    Hibernate CURD  operations checking

---
+++ HibProj4/src/com/test/DeleteTest.java
@@ -0,0 +1,29 @@
+package com.test;
+
+import org.hibernate.Session;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+import com.domain.Product;
+public class DeleteTest {
+
+	public static void main(String[] args) {
+/*	 //Configuration cfg=null;
+	 SessionFactory factory=null;
+	 Session ses=null;
+	 Product prod=null;
+	 Transaction tx=null;
+	 //Bootstrap hibernate..
+		Configuration cfg = new Configuration();  
+		cfg.configure("hibernate.cfg.xml"); 
+		
+	 //load cfg file..
+	 //cfg=cfg.configure("/com/cfgs/hibernate.cfg.xml");
+	 //System.out.println("hiii=="+cfg);
+	// cfg=cfg.configure("hibernate.cfg.xml");
+	 
+	 */
+	}
+}
